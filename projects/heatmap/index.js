4a74ee970e6c3ba0fe76eddee2b9c5218cc3f65a
Ira Sanchez <irasanchez@Iras-MacBook-Air.local>
2022-02-19 18:51:22 -0700

    Stories 1 and 2

---
+++ projects/heatmap/index.js
@@ -0,0 +1,14 @@
+import d3 from 'd3'
+// add a header
+var header = d3
+  .select("body")
+  .append("header")
+
+  header.append("h1")
+  .attr("id", "title")
+  .text("Monthly Global Land-Surface Temperature");
+heading;
+  header.append("h2")
+  .attr("id", "description")
+  .text("Monthly Global Land-Surface Temperature");
+heading;
